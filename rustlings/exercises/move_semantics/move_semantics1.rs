49d787dbf974195b028ffa21d1b22d660f6779c2
eteplus <eteplus@outlook.com>
2022-01-03 22:49:46 +0800

    fix: fix rustlings can not exec

--- rustlings/exercises/move_semantics/move_semantics1.rs
+++ rustlings/exercises/move_semantics/move_semantics1.rs
@@ -4,7 +4,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(&vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -13,12 +13,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = &vec;
+fn fill_vec(vec: Vec<i32>) -> &Vec<i32> {
+    let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec.to_vec()
+    &vec
 }
