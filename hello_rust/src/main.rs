b5f3d4a5c9fe483ba25e0297d07fdfe9f7127e24
eteplus <eteplus@outlook.com>
2022-01-13 13:10:49 +0800

    feat(hello_rust): disable dead_code lint

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,19 +1,20 @@
+#![allow(dead_code)]
 mod libs;
 use libs::*;
 
 fn main() {
     hello_cargo::run();
-    car::run();
-    car2::run();
-    car3::run();
-    car4::run();
-    fruit::run();
-    person::run();
-    file::run();
-    lifetime::run();
-    trait1::run();
-    trait2::run();
-    counter::run();
-    container::run();
-    groups::run();
+    // car::run();
+    // car2::run();
+    // car3::run();
+    // car4::run();
+    // fruit::run();
+    // person::run();
+    // file::run();
+    // lifetime::run();
+    // trait1::run();
+    // trait2::run();
+    // counter::run();
+    // container::run();
+    // groups::run();
 }
