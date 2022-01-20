5c5a4832c09227d9f3cd9872029408ad0cc81595
eteplus <eteplus@outlook.com>
2022-01-20 21:28:38 +0800

    feat(exercism): complete 'High Scores' exercise

---
+++ exercism/rust/high-scores/src/lib.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        HighScores {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores.as_slice()
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        return match self.scores.last() {
+            None => None,
+            Some(&n) => Some(n),
+        };
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut vec = self.scores.clone();
+        vec.sort();
+        vec.iter().rev().take(3).cloned().collect()
+    }
+}
