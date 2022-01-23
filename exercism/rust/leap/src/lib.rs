e8351ea4d170a798a14909d5a40ecb0a989b6acb
eteplus <eteplus@outlook.com>
2022-01-23 19:07:37 +0800

    feat(exercism): complete 'Leap' exercise

---
+++ exercism/rust/leap/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn is_leap_year(year: u64) -> bool {
+    if year % 4 != 0 {
+        return false;
+    }
+    if year % 100 != 0 {
+        return true;
+    }
+    if year % 400 == 0 {
+        return true;
+    }
+    return false;
+}
