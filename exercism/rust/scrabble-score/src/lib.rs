46f7df0dbddbcb2d579170be08be1b55110824d9
eteplus <eteplus@outlook.com>
2022-02-05 22:35:26 +0800

    feat(exercism): add 'Scrabble Score' other solution

--- exercism/rust/scrabble-score/src/lib.rs
+++ exercism/rust/scrabble-score/src/lib.rs
@@ -1,34 +1,18 @@
-use std::collections::HashMap;
-
-/// Compute the Scrabble score for a word.
-fn get_letter() -> HashMap<char, u64> {
-    HashMap::from_iter([
-        ('d', 2),
-        ('g', 2),
-        ('b', 3),
-        ('c', 3),
-        ('m', 3),
-        ('p', 3),
-        ('f', 4),
-        ('h', 4),
-        ('v', 4),
-        ('w', 4),
-        ('y', 4),
-        ('k', 5),
-        ('j', 8),
-        ('x', 8),
-        ('q', 10),
-        ('z', 10),
-    ])
+pub fn get_letter_value(c: char) -> u64 {
+    if !c.is_ascii_alphabetic() {
+        return 0;
+    }
+    match c {
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 1,
+    }
 }
 
 pub fn score(word: &str) -> u64 {
-    let letter = get_letter();
-    let mut sum = 0;
-    for c in word.to_lowercase().chars() {
-        if c.is_ascii_alphabetic() {
-            sum += letter.get(&c).unwrap_or(&1);
-        }
-    }
-    sum
+    word.to_lowercase().chars().map(get_letter_value).sum()
 }
